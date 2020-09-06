307145dcf509e3f43932b21b2467a1e6c2858147
Alecefe <ma.rodriguezf21@gmail.com>
2020-09-06 05:06:00 -0400

    Modulo 3 completo, estructura de cards, categories y logo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { GlobalStyles } from './GlobalStyles'
+import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
+
+export const App = () => (
+  <>
+    <Logo />
+    <GlobalStyles />
+    <ListOfCategories />
+    <ListOfPhotoCards />
+  </>
+)
